1fe1aecd7552a59e469e79a9c354fdc4ad0be556
Steve Shannon <srshannon@bis.midco.net>
2023-10-09 21:57:28 -0500

    Started on movement component for first person scene

---
+++ Assets/FirstPerson/FP_Movement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FP_Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private float acceleration = 1.0f;
+    [SerializeField] private float jumpForce = 1.0f;
+    [SerializeField] private float verticalForce = 0.0f;
+    [SerializeField] private bool grounded = false;
+    [SerializeField] private float gravity = 12.0f;
+    private bool jump = false;
+    private bool prevJump = false;
+    private Vector3 moveDir = Vector3.zero;
+    private Vector3 targetMoveDir = Vector3.zero;
+    private Rigidbody rbody;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        targetMoveDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        jump = Input.GetAxis("Jump") > 0;
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        grounded = false;
+
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 1.0f))
+        {
+            if (hit.transform.tag == "Ground")
+            {
+                grounded = true;
+            }
+        }
+
+        if (grounded)
+        {
+            moveDir = Vector3.MoveTowards(moveDir, targetMoveDir, acceleration);
+
+            verticalForce = 0.0f;
+            if (jump && !prevJump)
+            {
+                verticalForce = jumpForce;
+            }
+        }
+        else
+        {
+            verticalForce -= gravity;
+        }
+
+        rbody.velocity = moveDir.magnitude > 1.0f ? moveDir.normalized * moveSpeed : moveDir * moveSpeed;
+        rbody.velocity += new Vector3(0, verticalForce);
+
+        prevJump = jump;
+    }
+}
